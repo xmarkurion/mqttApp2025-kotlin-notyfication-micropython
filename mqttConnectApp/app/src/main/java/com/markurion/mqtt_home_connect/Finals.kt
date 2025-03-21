afc9874bec32dae084bdabf677f4b00e721d2e44
xmarkurion <xmarkurion@gmail.com>
2025-03-21 00:29:13 +0000

    clean

--- mqttConnectApp/app/src/main/java/com/markurion/mqtt_home_connect/Finals.kt
+++ mqttConnectApp/app/src/main/java/com/markurion/mqtt_home_connect/Finals.kt
@@ -2,9 +2,9 @@ package com.markurion.mqtt_home_connect
 
 class Finals {
     companion object {
-        const val brokerUrl = "tcp://broker.emqx.io:1883" // Replace with your broker URL
+        const val brokerUrl = "tcp://broker.emqx.io:1883"
         const val clientId = "a00300334-mobile"
         const val MQTTServiceChannel_ID = "MQTTServiceChannel"
-        const val topic = "a00300334/#" // Replace "your/topic" with your desired topic
+        const val topic = "a00300334/#"
     }
 }
